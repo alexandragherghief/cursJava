c1a281197a74ec5008572ab0e9f0d3bf0b7e76ba
Gherghief Alexandra <alexandra.gherghief@gmail.com>
2023-05-26 15:37:49 +0300

    curs5

---
+++ src/Curs5/DoWhileExample.java
@@ -0,0 +1,22 @@
+package Curs5;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] orase = {"Iasi", "Cluj", "Pitesti", "Oradea"};
+		//                   0       1         2          3
+		int i =0;
+		while(i > orase.length) {
+			System.out.println(orase[i]);
+			i++;
+		}
+		System.out.println("---------------------------");
+		int j = 0;
+		do {
+			System.out.println(orase[j]);
+			j++;
+		}while(j > orase.length);
+	}
+
+}
